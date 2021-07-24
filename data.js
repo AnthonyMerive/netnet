6faebf1339b9c89cfeebc34555165a97cf5fee0d
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-07-24 12:37:52 -0500

    subiendo data

---
+++ data.js
@@ -0,0 +1,162 @@
+
+export const ps5 = [
+    {
+        "id": 1,
+        "name": "game-1",
+        "game": "Assassin's Creed: Valhala",
+        "image": "",
+        "prize": "$62.000",
+    },
+    {
+        "id": 2,
+        "name": "game-2",
+        "game": "Call of Duty: Cold War",
+        "image": "",
+        "prize": "$48.000",
+    },
+    {
+        "id": 3,
+        "name": "game-3",
+        "game": "Farcry 6",
+        "image": "",
+        "prize": "$80.000",
+    },
+    {
+        "id": 4,
+        "name": "game-4",
+        "game": "Final Fantasy VII: Remake",
+        "image": "",
+        "prize": "$75.000",
+    },
+    {
+        "id": 5,
+        "name": "game-5",
+        "game": "Mortal Kombat 11",
+        "image": "",
+        "prize": "$46.000",
+    },
+    {
+        "id": 6,
+        "name": "game-6",
+        "game": "Rainbow Six Ciege: Extraction",
+        "image": "",
+        "prize": "$68.000",
+    },
+]
+
+
+export const xbox = [
+    {
+        "id": 1,
+        "name": "game-1",
+        "game": "Battlefield: 2042",
+        "image": "",
+        "prize": "$75.000",
+    },
+    {
+        "id": 2,
+        "name": "game-2",
+        "game": "Formula 1: 2021",
+        "image": "",
+        "prize": "$43.000",
+    },
+    {
+        "id": 3,
+        "name": "game-3",
+        "game": "Forza Horizon 5",
+        "image": "",
+        "prize": "$62.000",
+    },
+    {
+        "id": 4,
+        "name": "game-4",
+        "game": "Halo infinity",
+        "image": "",
+        "prize": "$79.000",
+    },
+    {
+        "id": 5,
+        "name": "game-5",
+        "game": "Metro Exodus",
+        "image": "",
+        "prize": "$48.000",
+    },
+    {
+        "id": 6,
+        "name": "game-6",
+        "game": "Resident Evil 8: Village",
+        "image": "",
+        "prize": "$75.000",
+    },
+]
+
+
+export const nintendo = [
+    {
+        "id": 1,
+        "name": "game-1",
+        "game": "Animal Crossing",
+        "image": "",
+        "prize": "$42.000",
+    },
+    {
+        "id": 2,
+        "name": "game-2",
+        "game": "Mario Kart 8 Deluxe",
+        "image": "",
+        "prize": "$48.000",
+    },
+    {
+        "id": 3,
+        "name": "game-3",
+        "game": "Mario Odissey",
+        "image": "",
+        "prize": "$55.000",
+    },
+    {
+        "id": 4,
+        "name": "game-4",
+        "game": "Pokemon Sword",
+        "image": "",
+        "prize": "$45.000",
+    },
+    {
+        "id": 5,
+        "name": "game-5",
+        "game": "Super Smash Bros Ultimate",
+        "image": "",
+        "prize": "$70.000",
+    },
+    {
+        "id": 6,
+        "name": "game-6",
+        "game": "Zelda Breath of the Wild",
+        "image": "",
+        "prize": "$68.000",
+    },
+]
+
+
+export const consolas = [
+    {
+        "id": 1,
+        "name": "console-1",
+        "game": "PlayStation 5",
+        "image": "",
+        "prize": "$3.000.000",
+    },
+    {
+        "id": 2,
+        "name": "console-2",
+        "game": "Xbox Series X",
+        "image": "",
+        "prize": "$2.500.000",
+    },
+    {
+        "id": 3,
+        "name": "console-3",
+        "game": "Nintendo Switch",
+        "image": "",
+        "prize": "$1.200.000",
+    },
+]
